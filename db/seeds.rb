1772fa5eb8af6432a381d8e983efc06706ba9b05
oagomezz <ogomezbarragan437@gmail.com>
2023-01-31 09:45:07 -0800

    added images to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,19 +4,19 @@ dogs = [
     name: 'Felix',
     age: 2,
     enjoys: 'Long naps on the couch, and a warm fire.',
-    image: ''
+    image: 'https://images.unsplash.com/photo-1608744882201-52a7f7f3dd60?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60'
   },
   {
     name: 'Homer',
     age: 12,
     enjoys: 'Food mostly, really just food.',
-    image: ''
+    image: 'https://images.unsplash.com/photo-1654994983327-d0f6fcde017b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80'
   },
   {
     name: 'Santi',
     age: 5,
     enjoys: 'Longs walks on the beach.',
-    image: ''
+    image: 'https://images.unsplash.com/photo-1547407139-3c921a66005c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
   }
 ]
 
